117234708571dc1881e0fc05917314e0cbcc729f
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-05-11 21:49:55 +0300

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './template-1/Footer'
+import Navbar from './template-1/Navbar'
+import PageContent from './template-1/PageContent'
+import Slide from './template-1/Slide'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return (<>
+    <Navbar></Navbar>
+    <Slide></Slide>
+    <PageContent></PageContent>
+    <Footer></Footer>
+  </>)
+
+
 }
 
 export default App;
---
+++ src/template-1/PageContent.js
@@ -0,0 +1,18 @@
+import About from "./About";
+import Contact from "./Contact";
+import Menu from "./Menu";
+
+
+function PageContent() {
+    return <>
+        <div className="w3-content" style={{ maxWidth: 1100 }}>
+
+            <About></About>
+            <Menu></Menu>
+            <Contact></Contact>
+        </div>
+
+    </>
+}
+
+export default PageContent
