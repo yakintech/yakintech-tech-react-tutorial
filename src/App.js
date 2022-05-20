ef4129e3c182271dff978263bd532a0be07772c2
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-05-20 13:08:41 +0300

    react router update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Route, Routes } from "react-router-dom"
 import AddCategory from "./fetchSample/AddCategory"
 import AddCategoryWithAxios from "./fetchSample/AddCategoryWithAxios"
 import UserList from "./fetchSample/UserList"
+import AboutPage from "./routerSample/AboutPage"
+import ContactPage from "./routerSample/ContactPage"
+import HomePage from "./routerSample/HomePage"
 
 function App() {
 
 
   return (<>
-    <AddCategoryWithAxios></AddCategoryWithAxios>
+<h1>Site Header</h1>
+    <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+      <Route path='/hakkimizda' element={<AboutPage/>}></Route>
+      <Route path='/iletisim' element={<ContactPage/>}></Route>
+    </Routes>
+<h1> Site Footer</h1>
   </>
   )
 }
